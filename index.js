e735a1d08224bfe290468acc1143e0003f98f2e0
Dave Butler <davebutlerid@gmail.com>
2021-04-03 22:45:32 -0600

    index.js boolean cleanup / loops.js revision

--- index.js
+++ index.js
@@ -1,41 +1,39 @@
+// Is it hot outside today?
 var isHotOutside = true;
 console.log('Is it hot outside? - ' + isHotOutside);
 
-var isWeekday = false;
+// Is today a weekday?
+var isWeekday = true;
 console.log('Is it a week day? - ' + isWeekday);
 
+// Do we have money on hand?
 var hasMoneyInPocket = true;
 console.log('Do I have money in my pocket? - ' + hasMoneyInPocket);
 
+// Cost of milk.
 var costOfMilk = 2.49;
 console.log('Cost of milk: $' + costOfMilk);
 
+// How much money in wallet?
 var moneyInWallet = 40;
 console.log('Money in wallet: $' + moneyInWallet);
 
+// How thirsty are we on 1 - 10 scale
 var thirstLevel = 5;
 console.log('Thirst level: ' + thirstLevel);
 
-var shouldBuyIceCream = false;
-if(isHotOutside && hasMoneyInPocket) {
-    shouldBuyIceCream = true;
-}
+//Buy ice cream if it is hot outside and we have money on hand.
+var shouldBuyIceCream = isHotOutside && hasMoneyInPocket;
 console.log('Should I buy ice cream? - ' + shouldBuyIceCream);
 
-var willGoSwimming = false;
-if(isHotOutside && isWeekday == false){
-    willGoSwimming = true;
-}
+//Go swimming if it is hot outside and it is not a weekday.
+var willGoSwimming = isHotOutside && !isWeekday;
 console.log('Will I go swimming? - ' + willGoSwimming);
 
-var isAGoodDay = false;
-if(isHotOutside && hasMoneyInPocket && isWeekday == false){
-    isAGoodDay = true;
-}
+//It's a good day if it is hot outside, we have money, and it is not a weekday.
+var isAGoodDay = isHotOutside && hasMoneyInPocket && !isWeekday;
 console.log('Is today a good day? - ' + isAGoodDay);
 
-var willBuyMilk = false;
-if(isHotOutside && thirstLevel >= 3 && moneyInWallet >= (2 * costOfMilk)){
-    willBuyMilk = true;
-}
+//Buy milk if it's hot outside, if we are thirsty and if we can afford 2 things of milk.
+var willBuyMilk = isHotOutside && thirstLevel >= 3 && moneyInWallet >= (2 * costOfMilk);
 console.log('Do I need to buy milk? - ' + willBuyMilk);
--- loops.js
+++ loops.js
@@ -1,7 +1,8 @@
-
+// Print all even numbers 0 - 100.
+console.log('Printing even numbers between 0 and 100...')
 var loopCount = 0;
 while(loopCount <= 100) {
-    
+    // Even numbers are numbers divisible by 2.
     if(loopCount % 2 == 0) {
         console.log(loopCount);
     }
@@ -9,6 +10,8 @@ while(loopCount <= 100) {
     loopCount++;
 }
 
+// Print every third number starting at 100.
+console.log('Printing every third number between 100 and 0...')
 var thirdNumCount = 100;
 while(thirdNumCount > 0) {    
     console.log(thirdNumCount);
@@ -16,24 +19,23 @@ while(thirdNumCount > 0) {
 
 }
 
+// Print every other number from 1 - 100
+console.log('Printing every other number from 1 to 100...')
 for(i = 1; i <= 100; i = i + 2) {
     console.log(i);
 }
 
-for(i = 1; i <= 100; i++) {
-    var outputText = '';
-
-    if(i % 3 == 0) {
-    outputText = 'Hello';
-    }
-
-    if(i % 5 == 0) {
-        outputText += 'World';
-    }
-
-    if(i % 3 && i % 5) {
+// Print every other number from 0 to 100 
+for(i = 0; i <= 100; i++) {
+    var outputText = i;
+    if(i % 3 == 0 && i % 5 == 0) {
+        outputText = 'HelloWorld';
+    } else if(i % 3 == 0 && i % 5 != 0) {   // Print Hello if number divisible by 3
+        outputText = 'Hello';
+    } else if(i % 5 == 0 && i % 3 != 0) {   // Print World if number divisible by 5
+        outputText = 'World';
+    } else {
         outputText = i;
     }
-
     console.log(outputText);
 }
