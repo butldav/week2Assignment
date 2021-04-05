3404c0e219982a1cec741712edfa0bc7b7138430
Dave Butler <davebutlerid@gmail.com>
2021-04-05 11:25:31 -0600

    final submission

--- loops.js
+++ loops.js
@@ -1,6 +1,7 @@
 // Print all even numbers 0 - 100.
 console.log('Printing even numbers between 0 and 100...')
 var loopCount = 0;
+
 while(loopCount <= 100) {
     // Even numbers are numbers divisible by 2.
     if(loopCount % 2 == 0) {
@@ -13,6 +14,7 @@ while(loopCount <= 100) {
 // Print every third number starting at 100.
 console.log('Printing every third number between 100 and 0...')
 var thirdNumCount = 100;
+
 while(thirdNumCount > 0) {    
     console.log(thirdNumCount);
     thirdNumCount = thirdNumCount - 3;
@@ -28,14 +30,16 @@ for(i = 1; i <= 100; i = i + 2) {
 // Print every other number from 0 to 100 
 for(i = 0; i <= 100; i++) {
     var outputText = i;
-    if(i % 3 == 0 && i % 5 == 0) {
+    
+    if(i % 3 == 0 && i % 5 == 0) { // Print HelloWorld if number divisible by both 3 and 5
         outputText = 'HelloWorld';
-    } else if(i % 3 == 0 && i % 5 != 0) {   // Print Hello if number divisible by 3
+    } else if(i % 3 == 0 && i % 5 != 0) {   // Print Hello if number divisible by 3 only
         outputText = 'Hello';
-    } else if(i % 5 == 0 && i % 3 != 0) {   // Print World if number divisible by 5
+    } else if(i % 5 == 0 && i % 3 != 0) {   // Print World if number divisible by 5 only
         outputText = 'World';
     } else {
         outputText = i;
     }
+
     console.log(outputText);
 }
